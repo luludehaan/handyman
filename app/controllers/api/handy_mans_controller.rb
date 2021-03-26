c34ca93aed288364ec2fb51dca5c07a03d1db377
Britt <Bargyle1@gmail.com>
2021-03-25 20:18:39 -0600

    Handy_man_controller

--- app/controllers/api/handy_mans_controller.rb
+++ app/controllers/api/handy_mans_controller.rb
@@ -1,2 +1,40 @@
 class Api::HandyMansController < ApplicationController
+  def index
+    render json: Handy_man.all
+  end
+
+  def show
+    @handy_man = Handy_man.find(params[:id])
+    render json: @handy_man
+  end
+  def create
+    @handy_man = Handy_man.new(handy_man_params)
+    if @handy_man.save
+      render json: @handy_man
+    else
+      render json: { errors: @handy_man.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update 
+    @handy_man = Handy_man.find(params[:id])
+    if @handy_man.update(handy_man_params)
+      render json: @handy_man
+    else
+      render json: { errors: @handy_man.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Handy_man.find(params[:id]).destroy
+    @handy_man.destroy
+    render json: {message: 'handy_man deleted'}
+  end
+
+  
+
+
+
+
+
 end
