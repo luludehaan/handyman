65f97685cfb1259dfdba4525452f1f089ee948d0
Sky B <skyburn5616@gmail.com>
2021-03-26 01:31:55 -0700

    ServicesJS partially complete with styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/shared/Home';
+import Services from './components/services/Services';
+import Comments from './components/comments/Comments';
 import NoMatch from './components/shared/NoMatch';
 import Navbar from './components/shared/Navbar';
 import Handymans from './components/handymans/Handymans';
@@ -12,6 +14,8 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/handymans" component={Handymans} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/comments" component={Comments} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -1,11 +1,12 @@
 import {Link} from 'react-router-dom';
 import {Menu} from 'semantic-ui-react';
+
 const Navbar = () => {
   return (
     <Menu>
       <Link to="/"><Menu.Item>Homepage</Menu.Item></Link>
-      <Link to="/handymans"><Menu.Item>Handydandymen</Menu.Item></Link>
       <Link to="/services"><Menu.Item>Our Services</Menu.Item></Link>
+      <Link to="/handymans"><Menu.Item>Handydandymen</Menu.Item></Link>
       <Link to="/comments"><Menu.Item>Comments</Menu.Item></Link>
     </Menu>
   )
