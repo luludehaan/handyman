84dd20baf758b9707d3a49f8ef7df43683bae44c
LuLu DeHaan <luludehaan@gmail.com>
2021-03-25 19:22:06 -0700

    home and no match

---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <h1>HandyHome</h1>
+      <h4>Welcome. Please select a handyman, service, or provide commentary on your experience.</h4>
+    </>
+  )
+}
+
+export default Home;
